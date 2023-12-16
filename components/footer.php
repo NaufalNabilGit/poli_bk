cde6d5d8997dd5b21a2181d164d67992efd853a1
NaufalNabilGit <125368259+NaufalNabilGit@users.noreply.github.com>
2023-12-16 11:11:02 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      2023 Poliklinik Bimbingan Karir
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Bill's</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
